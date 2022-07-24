ae14a8f5d550d83e0eb6fc7f0550d3f139b843bd
quadiez <dduidokdoi@gmail.com>
2022-07-24 13:11:11 +0700

    fix: meta

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import { theme } from './theme';
 
 import App from './App';
+import { Metatags } from './components/Metatags';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -16,6 +17,7 @@ root.render(
   <StrictMode>
     <BrowserRouter>
       <ChakraProvider theme={theme}>
+        <Metatags />
         <App />
       </ChakraProvider>
     </BrowserRouter>
